07e0624e4d4147c573c05e5c177f6ada128aa4e2
yahia-elshamy <y.ahmed0112000@gmail.com>
2026-06-21 19:17:14 +0300

    fixing the routes name in the server.js

--- backend/app.js
+++ backend/app.js
@@ -29,13 +29,13 @@ app.use("/uploads", express.static("uploads"));
 
 // Routes
 const authRoutes = require("./Routes/auth.routes");
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 const requestRoutes = require("./Routes/request.routes");
-app.use("api/requests", requestRoutes);
+app.use("/api/requests", requestRoutes);
 const applicationRoutes = require("./Routes/application.routes");
-app.use("api/applications", applicationRoutes);
+app.use("/api/applications", applicationRoutes);
 const reviewRoutes = require("./Routes/review.routes");
-app.use("api/reviews", requestRoutes);
+app.use("/api/reviews", requestRoutes);
 
 // Error Handler Middleware
 const errorHandler = require("./Middlewares/errorHandler.middleware");
